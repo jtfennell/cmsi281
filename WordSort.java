d2e6be1e642eb65ea5bfc7f22ab6a54dc69ba090
Jeff <jtfennell@hotmail.com>
2014-10-20 22:08:27 -0700

    created WordSort class

---
+++ WordSort.java
@@ -0,0 +1,20 @@
+import java.io;
+
+public class WordSort{
+
+	public static void main(String[] args) throws Exception{
+		boolean inputCaseSensitive = (args[0].equals("-sensitive");
+		
+		if (inputCaseSensitive) {
+				
+		}
+
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+		string s = stdIn.readLine();
+
+		while (s != null){
+
+		}
+	}
+}
