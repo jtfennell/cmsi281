e165495b5f577703a73a15459d4ea073b0dd121d
Jeff <jtfennell@hotmail.com>
2014-11-08 22:50:23 -0800

    changed switch statement to if statements, finished program

--- ExpressionEvaluator/ExpressionEvaluator.java
+++ ExpressionEvaluator/ExpressionEvaluator.java
@@ -1,39 +1,83 @@
 /**to add:
 -in catch statement- check to see if argument is one of the acceptable operators. throw new IllegalArgumentException() if it isn't
+-problem: illegal exception argument should be thrown at the beginning of the program
 
   */
 
 public class ExpressionEvaluator{
 
 	public static void main(String[] args) {
-		CustomizedStack stack = new CustomizedStack();
-		int currentValue = 0;
+        int numOfInts = 0;
+        int numOfOperators = 0;
 
-		for (int i = 0; i < args.length ; i++ ) {
-			//tries to parse the argument into an integer. If no exception is thrown, the arg is an integer
-			try{
-				int parsedInt = nextInteger.parseInt(args[i]);
+        //error checking to make sure all arguments are either operators or integers
+        for (int i = 0;i < args.length ;i++ ) {
+
+           if (isOperator(args[i])) {
+                if (numOfInts == 0) {
+                    throw new IllegalArgumentException("Attempts to pop empty stack");
+                }
+               else if (numOfInts > 1) {
+                   numOfInts--;
+               }
+           }
+           else {
+               try{
+                Integer.parseInt(args[i]);
+                numOfInts++;
+               }
+               catch (NumberFormatException e){
+                throw new IllegalArgumentException("Non-operator string detected");
+               }
+           }
+        }
+
+        CustomizedStack stack = new CustomizedStack();
+		int result;
+
+         //tries to parse the argument into an integer. If no exception is thrown, the arg is an integer and is added to the stack
+		for (int i = 0; i < args.length ; i++ ) {           
+            try{
+				int parsedInt = Integer.parseInt(args[i]);
 				stack.push(parsedInt);
 			}
-			//if a number format exception is thrown, the argument is checked to see if it is an operator, and evaluted if it is.
+			//if a number format exception is thrown, then the argument is an operator and expression is evaluated
 			catch (NumberFormatException e){
-				if (isOperator(args[i])) {
-					
-				}
-				value1 = stack.pop();
-				value2 = stack.pop();
-				stack.push(evaluate(value1, value2, args[i]));
+				
+				if (stack.size() > 1) {
+					int value1 = stack.pop();
+					int value2 = stack.pop();
+
+    				if (args[i].equals("+")) {
+                        stack.push(value2 + value1);
+                    }
+                    else if (args[i].equals("-")){
+                        stack.push(value2 - value1);
+                    }
+                    else if (args[i].equals("x")){
+                        stack.push(value2 * value1);
+                    }
+                    else if(args[i].equals("/")){
+                        stack.push(value2 / value1);
+                    }
+                    else if (args[i].equals("%")){
+                        stack.push(value2 % value1);
+                    }			
+                }
+				
 			}
 		}
+		
+        System.out.println(stack.pop());
 	}
 
 	public static boolean isOperator(String s){
-		boolean isOneCharacter = s.length == 1;
-		boolean isAddOperator = s.charAt(0).equals('+');
-		boolean isSubtractOperator = s.charAt(0).equals('-');
-		boolean isMultiplyOperator = s.charAt(0).equals('x');
-		boolean isDivideOperator = s.charAt(0).equals('/')
-		boolean isModOperator = s.charAt(0).equals('%');
+		boolean isOneCharacter = s.length() == 1;
+		boolean isAddOperator = s.equals("+");
+		boolean isSubtractOperator = s.equals("-");
+		boolean isMultiplyOperator = s.equals("x");
+		boolean isDivideOperator = s.equals("/");
+		boolean isModOperator = s.equals("%");
 
 		return (isOneCharacter &&(isAddOperator || isSubtractOperator || isMultiplyOperator || isDivideOperator || isModOperator));
 	}
