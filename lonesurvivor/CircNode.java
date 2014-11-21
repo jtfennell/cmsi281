834c8512e6e400019ba3d450c173a3af86b6ce74
Jeff <jtfennell@hotmail.com>
2014-11-20 18:48:03 -0800

    added javadoc comments

--- lonesurvivor/CircNode.java
+++ lonesurvivor/CircNode.java
@@ -1,3 +1,4 @@
+/**A custom node for a doubly-linked, circular list of integers */
 public class CircNode{
 	private CircNode previousNode;
 	private CircNode nextNode;
@@ -14,22 +15,27 @@ public class CircNode{
 		this.value = value;
 	}
 
+	/**Returns the pointer to the previous node in the list*/
 	public CircNode getPrevious(){
 		return previousNode;
 	}
 
+	/**Changes the reference to the previous node in the list*/
 	public void setPrevious(CircNode newPreviousNode){
 		previousNode = newPreviousNode;
 	}
 
+	/**Returns the pointer to the next node in the list */
 	public CircNode getNext(){
 		return nextNode;
 	}
 
+	/**changes the reference to the next node in the list*/
 	public void setNext(CircNode newNextNode){
 		nextNode = newNextNode;
 	}
 
+	/**returns the value of a node*/
 	public int value(){
 		return value;
 	}
