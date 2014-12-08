a9d231f8a7ae5c6cdb409c230bb4e0e3c524008d
Jeff <jtfennell@hotmail.com>
2014-12-07 23:36:15 -0800

    started Tree node

---
+++ BinaryTree/TreeNode.java
@@ -0,0 +1,20 @@
+public class TreeNode {
+	private Object value;
+	private TreeNode leftChild = null;
+	private TreeNode rightChild = null;
+	private TreeNode parentNode = null;
+
+	public TreeNode (Object value) {
+
+	}
+
+	public void setRightChild (TreeNode rightChild) {
+		this.rightChild = rightChild;
+	}
+
+	public void setLeftChild (TreeNode rightChild) {
+		this.leftChild = leftChild;
+	}
+
+
+}
