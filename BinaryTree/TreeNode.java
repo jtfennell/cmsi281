b2dce71c6114df6b0472d4d91ec398a11c43edf5
Jeff <jtfennell@hotmail.com>
2014-12-08 13:27:16 -0800

    removed child node objects

--- BinaryTree/TreeNode.java
+++ BinaryTree/TreeNode.java
@@ -1,22 +1,11 @@
 public class TreeNode {
-	private Object value;
-	private TreeNode leftChild = null;
-	private TreeNode rightChild = null;
-	private TreeNode parentNode = null;
+	private Object nodeData;
 
-	public TreeNode (Object value) {
-
+	public TreeNode (Object nodeData) {
+		this.nodeData = nodeData;
 	}
 
-	public void setRightChild (TreeNode rightChild) {
-		this.rightChild = rightChild;
-	}
-
-	public void setLeftChild (TreeNode rightChild) {
-		this.leftChild = leftChild;
-	}
-
-	public TreeNode getParent () {
-		return parentNode;
+	public void setData(Object nodeData){
+		this.nodeData = nodeData;
 	}
 }
