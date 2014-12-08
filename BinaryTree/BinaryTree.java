d999c38974f072eefdbf99aec5a7bcfbce005203
Jeff <jtfennell@hotmail.com>
2014-12-07 23:41:20 -0800

    started Binary Tree

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,118 @@
+import java.util.Collection;
+
+public class BinaryTree {
+	
+	public BinaryTree() {
+
+	}
+
+	public static createFromData(Object rootData, BinaryTree leftSubtree, BinaryTree rightSubtree) {
+
+	}
+
+	/**
+	 Adds a new node to the binary tree in inorder. Returns true if the tree changed as a result of the add.
+	*/
+	public boolean add() {
+
+	}
+
+	/**
+	Adds each of the elements in a collection to the tree.
+	*/
+	public boolean addAll() {
+
+	}
+
+	/**
+	Removes all of the nodes from the tree.
+	*/
+	public void clear() {
+
+	}
+
+	/**
+	Returns true if the tree contains the specified element.
+	*/
+	public boolean contains(Object o) {
+
+	}
+
+	/**
+	Returns true if the tree contains all of the elements in a collection.
+	*/
+	public boolean containsAll(Collection<?> c){
+
+	}
+
+	/**
+	Compares two trees to determine if they are equivalent.
+	*/
+	public boolean equals(BinaryTree treeToCompare) {
+
+	}
+
+	/**
+	Returns the hashcode value for the tree.
+	*/
+	public int hashCode() {
+
+	}
+
+	/**
+	Returns true if the tree contains no nodes.
+	*/
+	public boolean isEmpty() {
+
+	}
+
+	/**
+	Returns a preorder iterator over the tree.
+	*/
+	public Iterator iterator() {
+
+	}
+
+	/**
+	Removes a single node containing an instance of the specified object. Returns true if the collection changed as a result.
+	*/
+	public boolean remove() {
+
+	}
+
+	/**
+	Removes the nodes from the tree that are objects contained within the specified collection.
+	*/
+	public boolean removeAll() {
+
+	}
+
+	/** 
+	Retains only the elements in this collection that are in the specified collection.
+	*/
+	public boolean retainAll() {
+
+	}
+
+	/**
+	Returns the number of nodes currently in the tree.
+	*/
+	public int size() {
+
+	}
+
+	/**
+	Converts the binary tree into an array.
+	*/
+	public Object[] toArray() {
+
+	}
+
+	/**
+	Converts the binary tree into an array.
+	*/
+	public <T>T[] toArray() {
+
+	}
+}
+    
