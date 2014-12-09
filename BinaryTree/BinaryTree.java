46bdf4473e26cd878c1a298e0a22b247fabf94cf
Jeff <jtfennell@hotmail.com>
2014-12-08 22:53:54 -0800

    moved pointers to subtrees to treenode

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,33 +1,63 @@
 import java.util.Collection;
 
-public class BinaryTree {
+public class BinaryTree implements Collection {
+	private TreeNode root = null;
+	private TreeNode parentNode = null;
+	private TreeNode leftMostNode = null;
+	private boolean containsNodes = false;
 	
 	public BinaryTree() {
 
 	}
 
-	public static createFromData(Object rootData, BinaryTree leftSubtree, BinaryTree rightSubtree) {
+	public BinaryTree (Object rootData) {
+		root = new TreeNode(rootData);
+		containsNodes = true;
+	}
 
+	public static createFromData(Object rootData, BinaryTree leftSubtree, BinaryTree rightSubtree) {
+		root = new TreeNode(rootData);
+
+		root.setLeftSubtree(leftSubtree);
+		root.setRightSubtree(rightSubtree);
+
+		this.containsNodes = leftSubtree.containsNodes() || rightSubtree.containsNodes();
+		this.leftMostNode = leftSubtree.getLeftMost();
 	}
 
 	/**
-	 Adds a new node to the binary tree in inorder. Returns true if the tree changed as a result of the add.
+	 Adds a new node to the binary tree. Returns true if the tree changed as a result of the add.
 	*/
-	public boolean add() {
-
+	public boolean add(Object objectToAdd) {
+		if (root == null) {
+			root = new TreeNode();
+			root.setData(objectToAdd);
+			leftMostNode = root;
+			containsNodes = true;
+		}else {
+			newleftSubtree = new BinaryTree(objectToAdd);
+			newleftSubtree.
+		}
 	}
 
 	/**
 	Adds each of the elements in a collection to the tree.
 	*/
-	public boolean addAll() {
-
+	public boolean addAll(objectsToAdd) {
+		for (Object objectsToAdd : objectsToAdd){
+			this.add(objectToAdd);
+		}
+		containsNodes = true;
 	}
 
 	/**
 	Removes all of the nodes from the tree.
 	*/
 	public void clear() {
+		for (Object objectsToAdd : objectsToAdd){
+			this.remove(objectToAdd);
+		}
+		containsNodes = false;
 
 	}
 
@@ -41,7 +71,7 @@ public class BinaryTree {
 	/**
 	Returns true if the tree contains all of the elements in a collection.
 	*/
-	public boolean containsAll(Collection<?> c){
+	public boolean containsAll(Collection c){
 
 	}
 
@@ -70,7 +100,7 @@ public class BinaryTree {
 	Returns a preorder iterator over the tree.
 	*/
 	public Iterator iterator() {
-
+		return new BinaryTreeIterator(this);
 	}
 
 	/**
@@ -113,6 +143,21 @@ public class BinaryTree {
 	*/
 	public <T>T[] toArray() {
 
+	}
+
+	/**
+	Returns whether or not the tree contains any nodes
+	*/
+	public boolean containsNodes() {
+		return containsNodes;
+	}
+
+	public BinaryTree getLeftMost() {
+		return leftMostNode;
+	}
+
+	public void setParent(BinaryTree parentNode){
+		this.parentNode = parentNode;
 	}
 }
     
