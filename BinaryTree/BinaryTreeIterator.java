05314396a90be2906776f8c1c343bf23c35ca1a8
Jeff <jtfennell@hotmail.com>
2014-12-13 14:30:04 -0800

    started working on hasNext method

--- BinaryTree/BinaryTreeIterator.java
+++ BinaryTree/BinaryTreeIterator.java
@@ -1,7 +1,39 @@
-public class BinaryTreeIterator {
-	private Binary Tree targetTree;
+import java.util.AbstractCollection;
+import java.util.Iterator;
 
-	public BinaryTreeIterator (BinarTree targetTree){
+public class BinaryTreeIterator implements Iterator {
+	private BinaryTree targetTree;
+	private int numberOfNodesSeen = 0;
+	private BinaryTree currentNode;
+
+	public BinaryTreeIterator (BinaryTree targetTree){
 		this.targetTree = targetTree;
+
+		if (targetTree.size() >= 1) {
+			currentNode = targetTree.getLeftMost();
+		}
+	}
+
+	public boolean hasNext () {
+
+		boolean hasNext = false;
+
+		if (targetTree.size() < 2) {
+			hasNext = false;
+		}else if (targetTree) {
+
+		}
+
+		return hasNext;
+	}
+
+	public BinaryTree next() {
+		BinaryTree nextTreeNode = new BinaryTree();
+
+		return nextTreeNode;
+	}
+
+	public void remove() {
+		
 	}
 }
