b390e49325a4a2acb0749add5de79bdefd6b5149
Jeff <jtfennell@hotmail.com>
2015-02-22 20:19:53 -0800

    fix spacing and indentation

--- ExpressionEvaluator/ExpressionEvaluator.java
+++ ExpressionEvaluator/ExpressionEvaluator.java
@@ -1,4 +1,4 @@
-public class ExpressionEvaluator{
+public class ExpressionEvaluator {
     /**Evaluates expression in reverse Polish notation. Uses operators +, -, x, /, %*/
 	public static void main(String[] args) {
         int numOfInts = 0;
@@ -7,23 +7,22 @@ public class ExpressionEvaluator{
         //error checking to make sure all arguments are either operators or integers
         for (int i = 0;i < args.length ;i++ ) {
 
-           if (isOperator(args[i])) {
+            if (isOperator(args[i])) {
                 if (numOfInts == 0) {
                     throw new IllegalArgumentException("Attempts to pop empty stack");
                 }
                else if (numOfInts > 1) {
                    numOfInts--;
                }
-           }
-           else {
+            } else {
                 try{
-                Integer.parseInt(args[i]);
-                numOfInts++;
+                    Integer.parseInt(args[i]);
+                    numOfInts++;
                 }
                 catch (NumberFormatException e){
-                throw new IllegalArgumentException("Non-operator string detected");
-               }
-           }
+                    throw new IllegalArgumentException("Non-operator string detected");
+                }
+            }
         }
 
         CustomizedStack stack = new CustomizedStack();
@@ -37,24 +36,20 @@ public class ExpressionEvaluator{
 			}
 			//if a number format exception is thrown, then the argument is an operator and expression is evaluated
             //if there is only 1 item in the stack, the operator does nothing to the stack
-			catch (NumberFormatException e){
+			catch (NumberFormatException e) {
 				if (stack.size() > 1) {
 					int value1 = stack.pop();
 					int value2 = stack.pop();
 
     				if (args[i].equals("+")) {
                         stack.push(value2 + value1);
-                    }
-                    else if (args[i].equals("-")){
+                    } else if (args[i].equals("-")){
                         stack.push(value2 - value1);
-                    }
-                    else if (args[i].equals("x")){
+                    } else if (args[i].equals("x")){
                         stack.push(value2 * value1);
-                    }
-                    else if(args[i].equals("/")){
+                    } else if(args[i].equals("/")){
                         stack.push(value2 / value1);
-                    }
-                    else if (args[i].equals("%")){
+                    } else if (args[i].equals("%")){
                         stack.push(value2 % value1);
                     }			
                 }
