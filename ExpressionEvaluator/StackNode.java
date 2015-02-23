a84b2602d2ec326021661ecf852829c3ebf9e80f
Jeff <jtfennell@hotmail.com>
2015-02-22 20:17:08 -0800

    fix spacing of braces

--- ExpressionEvaluator/StackNode.java
+++ ExpressionEvaluator/StackNode.java
@@ -1,33 +1,33 @@
-public class StackNode{
+public class StackNode {
 	StackNode previousNode;
 	int value;
 
 	/**Constructor that sets both the value and the previous Node */
-	public StackNode(int value, StackNode previousNode){
+	public StackNode(int value, StackNode previousNode) {
 		this.value = value;
 		this.previousNode = previousNode;
 	}
 
 	/**Constructor that sets only the value of the Node*/
-	public StackNode (int value){
+	public StackNode (int value) {
 		this.value = value;
 	}
 
 	/**Instantiates an empty stack*/
-	public StackNode(){
+	public StackNode() {
 	}
 
 	/**Sets the reference to the previous Node*/
-	public void setPrevious(StackNode previousNode){
+	public void setPrevious(StackNode previousNode) {
 		this.previousNode = previousNode;
 	}
 	
 	/**Returns the reference to the node underneath in the stack*/
-	public StackNode getPrevious(){
+	public StackNode getPrevious() {
 		return previousNode;
 	}
 	/**returns the integer value of the Node*/
-	public int value(){
+	public int value() {
 		return value;
 	}
 }
