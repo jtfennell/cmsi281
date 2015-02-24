2e1d16d1565f0c863c0e5a2cc99fe56ba0ad6628
Jeff <jtfennell@hotmail.com>
2015-02-24 14:01:01 -0800

    convert tabs to spaces

--- WordCounter.java
+++ WordCounter.java
@@ -1,27 +1,27 @@
 public class WordCounter implements Comparable<WordCounter>{
-	private String word;
-	private int numberOfAppearances = 1;
+    private String word;
+    private int numberOfAppearances = 1;
 
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        
+    }
 
-	public WordCounter(String word){
-		this.word = word;
-	}
+    public WordCounter(String word){
+        this.word = word;
+    }
 
-	public void incrementAppearances(){
-		numberOfAppearances++;
-	}
-	public String toString(){
-		return word;
-	}
+    public void incrementAppearances(){
+        numberOfAppearances++;
+    }
+    public String toString(){
+        return word;
+    }
 
-	public int getNumberOfAppearances(){
-		return numberOfAppearances;
-	}
+    public int getNumberOfAppearances(){
+        return numberOfAppearances;
+    }
 
-	public int compareTo(WordCounter otherWord){
-		return word.toUpperCase().compareTo(otherWord.word.toUpperCase());
-	}
+    public int compareTo(WordCounter otherWord){
+        return word.toUpperCase().compareTo(otherWord.word.toUpperCase());
+    }
 }
