3d41f61a559c14178650b7c654fcab473b1694b2
Jeff <jtfennell@hotmail.com>
2015-02-24 13:57:10 -0800

    convert indentation to spaces

--- ExpressionEvaluator/CustomizedStack.java
+++ ExpressionEvaluator/CustomizedStack.java
@@ -1,39 +1,39 @@
 public class CustomizedStack {
-	private StackNode topOfStack = new StackNode();
-	private int itemsInStack = 0;
+    private StackNode topOfStack = new StackNode();
+    private int itemsInStack = 0;
 
-	/**Instantiates an empty stack*/
-	public CustomizedStack() {
-	}
+    /**Instantiates an empty stack*/
+    public CustomizedStack() {
+    }
 
-	/**Adds a new number to the top of the stack*/
-	public void push(int nodeValue) {
-		StackNode newTop = new StackNode(nodeValue);
+    /**Adds a new number to the top of the stack*/
+    public void push(int nodeValue) {
+        StackNode newTop = new StackNode(nodeValue);
 
-		if (itemsInStack > 0) {
-			newTop.setPrevious(topOfStack);
-		} else {
-			newTop.setPrevious(null);
-		}
+        if (itemsInStack > 0) {
+            newTop.setPrevious(topOfStack);
+        } else {
+            newTop.setPrevious(null);
+        }
 
-		topOfStack = newTop;
-		itemsInStack++;
-	}
+        topOfStack = newTop;
+        itemsInStack++;
+    }
 
-	/**Removes the top value from the stack and returns it */
-	public int pop() throws IndexOutOfBoundsException {
-		if (itemsInStack == 0) {
-			throw new IndexOutOfBoundsException();
-		}
+    /**Removes the top value from the stack and returns it */
+    public int pop() throws IndexOutOfBoundsException {
+        if (itemsInStack == 0) {
+            throw new IndexOutOfBoundsException();
+        }
 
-		StackNode topNode = topOfStack;
-		topOfStack = topOfStack.getPrevious();
-		itemsInStack--;
-		
-		return topNode.value();
-	}
+        StackNode topNode = topOfStack;
+        topOfStack = topOfStack.getPrevious();
+        itemsInStack--;
+        
+        return topNode.value();
+    }
 
-	public int size() {
-		return itemsInStack;
-	}
+    public int size() {
+        return itemsInStack;
+    }
 }
